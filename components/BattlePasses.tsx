8e2631bc2ca342d797c96329db872a78db87bd37
Andrew Gaskell <gskll@protonmail.com>
2022-01-04 16:28:20 +0100

    feat: add Apollo to app with boilerplate
    - fetch all battle passes and display on /

--- components/BattlePasses.tsx
+++ components/BattlePasses.tsx
@@ -1,11 +1,42 @@
+import { gql, useQuery } from '@apollo/client'
+import styled from 'styled-components'
 import Button from './styles/Button'
 
+export const ALL_BATTLEPASSES_QUERY = gql`
+  query ALL_BATTLEPASSES_QUERY {
+    battlePasses {
+      id
+      name
+      status
+      start_date
+      end_date
+    }
+  }
+`
+
+const BattlePassGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 60px;
+`
+
 const BattlePasses = () => {
+  const { data, error, loading } = useQuery(ALL_BATTLEPASSES_QUERY)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
   return (
     <div>
       <h1>All Battle Passes</h1>
       <Button>+ Create new battle pass</Button>
-      <div>BattlePass List</div>
+      <div>
+        <BattlePassGridStyles>
+          {data.battlePasses.map((bp) => (
+            <div key={bp.id}>{bp.name}</div>
+          ))}
+        </BattlePassGridStyles>
+      </div>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
-import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { AppProps } from 'next/app'
 import Page from '../components/Page'
+import withData from '../lib/withData'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, apollo }: AppProps) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   )
 }
 
-export default MyApp
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {}
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+  pageProps.query = ctx.query
+  return { pageProps }
+}
+
+export default withData(MyApp)
