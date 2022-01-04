fcdfbe26a6a74217a2dc96338dfff84e78a95071
Andrew Gaskell <gskll@protonmail.com>
2022-01-04 21:08:48 +0100

    types: add type definitions for data

--- types.ts
+++ types.ts
@@ -5,3 +5,44 @@ export interface BattlePassOverview {
   start_date: string
   end_date: string
 }
+
+export interface BattlePass {
+  name: string
+  description: string
+  status: string
+  start_date: string
+  end_date: string
+  experience: string
+  tiers: Tier[]
+  missions: Mission[]
+  rewards: Reward[]
+}
+
+export interface Tier {
+  id: string
+  name: string
+  price: string
+  levels: Level[]
+}
+
+export interface Level {
+  id: string
+  name: string
+  reward: Reward[]
+}
+
+export interface Reward {
+  id: string
+  name: string
+  description: string
+  type: string
+  rarity: string
+}
+
+export interface Mission {
+  id: string
+  name: string
+  description: string
+  type: string
+  goal_type: string
+}
